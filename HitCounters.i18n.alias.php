018345f3cd6b427baa606fa91fce9eed572d9a23
Timo Tijhof <krinklemail@gmail.com>
2015-06-27 04:14:38 +0100

    Fix missing i18n aliases for special page 'PopularPages'
    From the erorr log:
    > [warning] Did not find alias for special page 'PopularPages'.
    > Perhaps no aliases are defined for it? [Called from SpecialPageFactory::getLocalNameFor]
    
    Change-Id: I4a4ba88fadc73a7895dc8b9cd7c2acf95bac044a

---
+++ HitCounters.i18n.alias.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Internationalization for Special page names
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$specialPageAliases = array();
+
+/** English (English) */
+$specialPageAliases['en'] = array(
+	'PopularPages' => array( 'PopularPages' ),
+);
