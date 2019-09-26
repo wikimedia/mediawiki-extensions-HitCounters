b4e9e6035d3716ad0ff40c16e5e35f00bf1952c2
WikiMANNia <55834825+WikiMANNia@users.noreply.github.com>
2019-09-26 22:18:54 +0200

    change "array()" to "[]"

--- HitCounters.i18n.magic.php
+++ HitCounters.i18n.magic.php
@@ -3,164 +3,166 @@
  * Internationalization file for magic words.
  */
 
-$magicWords = array();
+$magicWords = [];
 
 /** English (English) */
-$magicWords['en'] = array(
-	'numberofviews' => array( 0, 'NUMBEROFVIEWS' ),
-);
+$magicWords['en'] = [
+	'numberofviews' => [ 0, 'NUMBEROFVIEWS' ],
+	'numberofpageviews' => [ 0, 'NUMBEROFPAGEVIEWS' ],
+];
 
 /** Egyptian Arabic (مصرى) */
-$magicWords['arz'] = array(
-	'numberofviews' => array( 0, 'عدد_المشاهدات' ),
-);
+$magicWords['arz'] = [
+	'numberofviews' => [ 0, 'عدد_المشاهدات' ],
+];
 
 /** Bulgarian (български) */
-$magicWords['bg'] = array(
-	'numberofviews' => array( 0, 'БРОЙПРЕГЛЕДИ' ),
-);
+$magicWords['bg'] = [
+	'numberofviews' => [ 0, 'БРОЙПРЕГЛЕДИ' ],
+];
 
 /** Chechen (нохчийн) */
-$magicWords['ce'] = array(
-	'numberofviews' => array( 0, 'ХЬАЖАРИЙ_ДУКХАЛЛА', 'КОЛИЧЕСТВО_ПРОСМОТРОВ' ),
-);
+$magicWords['ce'] = [
+	'numberofviews' => [ 0, 'ХЬАЖАРИЙ_ДУКХАЛЛА', 'КОЛИЧЕСТВО_ПРОСМОТРОВ' ],
+];
 
 /** German (Deutsch) */
-$magicWords['de'] = array(
-	'numberofviews' => array( 0, 'BETRACHTUNGEN' ),
-);
+$magicWords['de'] = [
+	'numberofviews' => [ 0, 'AUFRUFE', 'ANSICHTEN', 'BETRACHTUNGEN' ],
+	'numberofpageviews' => [ 0, 'SEITENAUFRUFE', 'SEITENANSICHTEN', 'SEITENBETRACHTUNGEN' ],
+];
 
 /** Zazaki (Zazaki) */
-$magicWords['diq'] = array(
-	'numberofviews' => array( 0, 'AMARİYAMOCNAYIŞAN' ),
-);
+$magicWords['diq'] = [
+	'numberofviews' => [ 0, 'AMARİYAMOCNAYIŞAN' ],
+];
 
 /** Greek (Ελληνικά) */
-$magicWords['el'] = array(
-	'numberofviews' => array( 0, 'ΑΡΙΘΜΟΣΕΜΦΑΝΙΣΕΩΝ' ),
-);
+$magicWords['el'] = [
+	'numberofviews' => [ 0, 'ΑΡΙΘΜΟΣΕΜΦΑΝΙΣΕΩΝ' ],
+];
 
 /** Spanish (español) */
-$magicWords['es'] = array(
-	'numberofviews' => array( 0, 'NÚMERODEVISTAS', 'NUMERODEVISTAS' ),
-);
+$magicWords['es'] = [
+	'numberofviews' => [ 0, 'NÚMERODEVISTAS', 'NUMERODEVISTAS' ],
+];
 
 /** Estonian (eesti) */
-$magicWords['et'] = array(
-	'numberofviews' => array( 0, 'VAATAMISTEARV' ),
-);
+$magicWords['et'] = [
+	'numberofviews' => [ 0, 'VAATAMISTEARV' ],
+];
 
 /** Finnish (suomi) */
-$magicWords['fi'] = array(
-	'numberofviews' => array( 0, 'SIVUHAKUMÄÄRÄ' ),
-);
+$magicWords['fi'] = [
+	'numberofviews' => [ 0, 'SIVUHAKUMÄÄRÄ' ],
+];
 
 /** French (français) */
-$magicWords['fr'] = array(
-	'numberofviews' => array( 0, 'NOMBREVUES' ),
-);
+$magicWords['fr'] = [
+	'numberofviews' => [ 0, 'NOMBREVUES' ],
+];
 
 /** Galician (galego) */
-$magicWords['gl'] = array(
-	'numberofviews' => array( 0, 'NÚMERODEVISITAS' ),
-);
+$magicWords['gl'] = [
+	'numberofviews' => [ 0, 'NÚMERODEVISITAS' ],
+];
 
 /** Hawaiian (Hawai`i) */
-$magicWords['haw'] = array(
-	'numberofviews' => array( 0, 'HELUNĀNĀ' ),
-);
+$magicWords['haw'] = [
+	'numberofviews' => [ 0, 'HELUNĀNĀ' ],
+];
 
 /** Hindi (हिन्दी) */
-$magicWords['hi'] = array(
-	'numberofviews' => array( 0, 'दृश्यता_संख्या' ),
-);
+$magicWords['hi'] = [
+	'numberofviews' => [ 0, 'दृश्यता_संख्या' ],
+];
 
 /** Italian (italiano) */
-$magicWords['it'] = array(
-	'numberofviews' => array( 0, 'NUMEROVISITE' ),
-);
+$magicWords['it'] = [
+	'numberofviews' => [ 0, 'NUMEROVISITE' ],
+];
 
 /** Japanese (日本語) */
-$magicWords['ja'] = array(
-	'numberofviews' => array( 0, '閲覧回数' ),
-);
+$magicWords['ja'] = [
+	'numberofviews' => [ 0, '閲覧回数' ],
+];
 
 /** Cornish (kernowek) */
-$magicWords['kw'] = array(
-	'numberofviews' => array( 0, 'NIVERAWELYANSOW' ),
-);
+$magicWords['kw'] = [
+	'numberofviews' => [ 0, 'NIVERAWELYANSOW' ],
+];
 
 /** Malayalam (മലയാളം) */
-$magicWords['ml'] = array(
-	'numberofviews' => array( 0, 'എടുത്തുനോക്കലുകളുടെണ്ണം' ),
-);
+$magicWords['ml'] = [
+	'numberofviews' => [ 0, 'എടുത്തുനോക്കലുകളുടെണ്ണം' ],
+];
 
 /** Marathi (मराठी) */
-$magicWords['mr'] = array(
-	'numberofviews' => array( 0, 'धडकसंख्या', 'प्रेक्षासंख्या' ),
-);
+$magicWords['mr'] = [
+	'numberofviews' => [ 0, 'धडकसंख्या', 'प्रेक्षासंख्या' ],
+];
 
 /** Mazanderani (مازِرونی) */
-$magicWords['mzn'] = array(
-	'numberofviews' => array( 0, 'تعدادبازدید', 'NUMBEROFVIEWS' ),
-);
+$magicWords['mzn'] = [
+	'numberofviews' => [ 0, 'تعدادبازدید', 'NUMBEROFVIEWS' ],
+];
 
 /** Low Saxon (Netherlands) (Nedersaksies) */
-$magicWords['nds-nl'] = array(
-	'numberofviews' => array( 0, 'ANTALKERENBEKEKEN' ),
-);
+$magicWords['nds-nl'] = [
+	'numberofviews' => [ 0, 'ANTALKERENBEKEKEN' ],
+];
 
 /** Norwegian Nynorsk (norsk nynorsk) */
-$magicWords['nn'] = array(
-	'numberofviews' => array( 0, 'VISINGSTAL', 'TALPÅVISINGAR' ),
-);
+$magicWords['nn'] = [
+	'numberofviews' => [ 0, 'VISINGSTAL', 'TALPÅVISINGAR' ],
+];
 
 /** Portuguese (português) */
-$magicWords['pt'] = array(
-	'numberofviews' => array( 0, 'NUMERODEEXIBICOES', 'NÚMERODEEXIBIÇÕES' ),
-);
+$magicWords['pt'] = [
+	'numberofviews' => [ 0, 'NUMERODEEXIBICOES', 'NÚMERODEEXIBIÇÕES' ],
+];
 
 /** Brazilian Portuguese (português do Brasil) */
-$magicWords['pt-br'] = array(
-	'numberofviews' => array( 0, 'NUMERODEEXIBICOES', 'NÚMERODEEXIBIÇÕES' ),
-);
+$magicWords['pt-br'] = [
+	'numberofviews' => [ 0, 'NUMERODEEXIBICOES', 'NÚMERODEEXIBIÇÕES' ],
+];
 
 /** Quechua (Runa Simi) */
-$magicWords['qu'] = array(
-	'numberofviews' => array( 0, 'HAYKAQHAWASQA', 'HAYKAQAWASQA', 'NÚMERODEVISTAS', 'NUMERODEVISTAS' ),
-);
+$magicWords['qu'] = [
+	'numberofviews' => [ 0, 'HAYKAQHAWASQA', 'HAYKAQAWASQA', 'NÚMERODEVISTAS', 'NUMERODEVISTAS' ],
+];
 
 /** Russian (русский) */
-$magicWords['ru'] = array(
-	'numberofviews' => array( 0, 'КОЛИЧЕСТВО_ПРОСМОТРОВ' ),
-);
+$magicWords['ru'] = [
+	'numberofviews' => [ 0, 'КОЛИЧЕСТВО_ПРОСМОТРОВ' ],
+];
 
 /** Serbo-Croatian (srpskohrvatski / српскохрватски) */
-$magicWords['sh'] = array(
-	'numberofviews' => array( 0, 'BROJPREGLEDA' ),
-);
+$magicWords['sh'] = [
+	'numberofviews' => [ 0, 'BROJPREGLEDA' ],
+];
 
 /** Serbian (Latin script) (srpski (latinica)‎) */
-$magicWords['sr-el'] = array(
-	'numberofviews' => array( 0, 'BROJPREGLEDA', 'BROJ_PREGLEDA' ),
-);
+$magicWords['sr-el'] = [
+	'numberofviews' => [ 0, 'BROJPREGLEDA', 'BROJ_PREGLEDA' ],
+];
 
 /** Talysh (толышә зывон) */
-$magicWords['tly'] = array(
-	'numberofviews' => array( 0, 'ДИЈӘ_КАРДЕ_ҒӘДӘР' ),
-);
+$magicWords['tly'] = [
+	'numberofviews' => [ 0, 'ДИЈӘ_КАРДЕ_ҒӘДӘР' ],
+];
 
 /** Ukrainian (українська) */
-$magicWords['uk'] = array(
-	'numberofviews' => array( 0, 'КІЛЬКІСТЬ_ПЕРЕГЛЯДІВ' ),
-);
+$magicWords['uk'] = [
+	'numberofviews' => [ 0, 'КІЛЬКІСТЬ_ПЕРЕГЛЯДІВ' ],
+];
 
 /** Uzbek (oʻzbekcha/ўзбекча) */
-$magicWords['uz'] = array(
-	'numberofviews' => array( 0, 'KORISHSONI' ),
-);
+$magicWords['uz'] = [
+	'numberofviews' => [ 0, 'KORISHSONI' ],
+];
 
 /** Vietnamese (Tiếng Việt) */
-$magicWords['vi'] = array(
-	'numberofviews' => array( 0, 'SỐ_LẦN_XEM', 'SỐLẦNXEM' ),
-);
+$magicWords['vi'] = [
+	'numberofviews' => [ 0, 'SỐ_LẦN_XEM', 'SỐLẦNXEM' ],
+];
