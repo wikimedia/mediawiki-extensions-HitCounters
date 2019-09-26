a3bc79f9ae50a52f0eaab70a48df39dbc6538542
MaWoScha <manfredwormschaefer@gmail.com>
2019-09-26 20:50:21 +0200

    Add more translations into 'HitCounters.i18n.alias.php'

--- HitCounters.i18n.alias.php
+++ HitCounters.i18n.alias.php
@@ -17,3 +17,33 @@ $specialPageAliases['en'] = [
 $specialPageAliases['de'] = [
 	'PopularPages' => [ 'Beliebteste_Seiten' ],
 ];
+
+/** Spanish (Español) */
+$specialPageAliases['es'] = [
+	'PopularPages' => [ 'PáginasMásVisitadas' ],
+];
+
+/** French (Français) */
+$specialPageAliases['fr'] = [
+	'PopularPages' => [ 'Pages_les_plus_visitées' ],
+];
+
+/** Italian (Italiano) */
+$specialPageAliases['it'] = [
+	'PopularPages' => [ 'PaginePiùVisitate' ],
+];
+
+/** Dutch (Nederlands) */
+$specialPageAliases['nl'] = [
+	'PopularPages' => [ 'PopulairePaginas' ],
+];
+
+/** Portuguese (Português) */
+$specialPageAliases['nl'] = [
+	'PopularPages' => [ 'Páginas_populares' ],
+];
+
+/** Russian (русский) */
+$specialPageAliases['ru'] = [
+	'PopularPages' => [ 'Популярные_страницы' ],
+];
