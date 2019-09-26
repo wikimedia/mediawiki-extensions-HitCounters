8e13d44c73e1214f6103a64c05c1f6750c496c56
MaWoScha <manfredwormschaefer@gmail.com>
2019-09-26 23:44:15 +0200

    Update some more stuff

--- HitCounters.i18n.alias.php
+++ HitCounters.i18n.alias.php
@@ -12,3 +12,38 @@ $specialPageAliases = array();
 $specialPageAliases['en'] = array(
 	'PopularPages' => array( 'PopularPages' ),
 );
+
+/** German (Deutsch) */
+$specialPageAliases['de'] = array(
+	'PopularPages' => array( 'Beliebteste_Seiten' ),
+);
+
+/** Spanish (Español) */
+$specialPageAliases['es'] = array(
+	'PopularPages' => array( 'PáginasMásVisitadas' ),
+);
+
+/** French (Français) */
+$specialPageAliases['fr'] = array(
+	'PopularPages' => array( 'Pages_les_plus_visitées' ),
+);
+
+/** Italian (Italiano) */
+$specialPageAliases['it'] = array(
+	'PopularPages' => array( 'PaginePiùVisitate' ),
+);
+
+/** Dutch (Nederlands) */
+$specialPageAliases['nl'] = array(
+	'PopularPages' => array( 'PopulairePaginas' ),
+);
+
+/** Portuguese (Português) */
+$specialPageAliases['nl'] = array(
+	'PopularPages' => array( 'Páginas_populares' ),
+);
+
+/** Russian (русский) */
+$specialPageAliases['ru'] = array(
+	'PopularPages' => array( 'Популярные_страницы' ),
+);
