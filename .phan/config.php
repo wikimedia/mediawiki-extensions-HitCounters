aabfd9d4e2fc675e2237344146721f9690e06b92
Mark A. Hershberger <mah@nichework.com>
2023-11-23 21:54:21 -0500

    Clean up phan
    Change-Id: I1d0d1472fe2fe3827fb58f9f2d1f25a20292c4b0

--- .phan/config.php
+++ .phan/config.php
@@ -1,20 +1,34 @@
 <?php
+//phpcs:ignoreFile
+// The throws here cause a problem for phpcs.
 
-$cfg = require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config.php';
+$mwp = getenv( "MW_INSTALL_PATH" ) ?: realpath( __DIR__ . "/../../.." );
+if ( $mwp === false ) {
+	 throw new Exception( "No MediaWiki root found!" );
+}
+$ven = realpath( __DIR__ . "/../vendor" ) ?: realpath( "$mwp/vendor" );
+if ( $ven === false ) {
+	 throw new Exception( "No vendor path found!" );
+}
+$ext = getenv( "MW_EXTENSION_PATH" ) ?: "$mwp/extensions";
+$cfg = require "$ven/mediawiki/mediawiki-phan-config/src/config.php";
 
-$cfg['directory_list'] = array_merge(
-	$cfg['directory_list'],
-	[
-		'includes',
-		"../../extensions/AbuseFilter"
-	]
+$extensions = [ "AbuseFilter" ];
+$dirList = array_filter(
+	array_map( fn ( $dir ): string => "$ext/$dir", $extensions ),
+	fn ( $dir ): bool => is_dir( $dir )
 );
 
+$removeDir = array_filter(
+	array_map( fn ( $dir ): string => basename( $dir ), $dirList ),
+	fn ( $dir ): bool => is_dir( __DIR__ . "/stubs/$dir" )
+);
+
+$cfg['directory_list'][] = "includes";
+$cfg['directory_list'] = array_merge( $cfg['directory_list'], $dirList );
+
 $cfg['exclude_analysis_directory_list'] = array_merge(
-	$cfg['exclude_analysis_directory_list'], [
-		getenv( "MW_INSTALL_PATH" ) ?: "",
-		"../../extensions/AbuseFilter"
-	]
+	$cfg['exclude_analysis_directory_list'] ?? [], [ $mwp ], $dirList
 );
 
 return $cfg;
